93b96b96c5ae2de725aa143284c28e572a36a0c9
Bradley Kars <Bradley.kars@gmail.com>
2023-09-18 14:04:52 -0400

    Initial commit

---
+++ Day 001.py
@@ -0,0 +1,12 @@
+print("Bradley Kars")
+print("September 19th 2023")
+print("""
+I am committed to taking on and successfully completing the 100 Days of Code challenge. Over the course of this journey, I aim to enhance my coding skills, expand my knowledge, and build meaningful projects. Through consistency and dedication, I look forward to achieving my coding goals and becoming a better developer.
+ 
+As I embark on this coding journey, starting from Day 1 of 100, I want to extend my heartfelt thanks to everyone who has been following along with my progress.
+
+Your support and encouragement have already meant a lot, and I truly appreciate it.
+Here's to more coding adventures ahead!
+""")
+print("I am feeling 🥳")
+print("Please follow my progress at https://github.com/Bradley-Kars")
