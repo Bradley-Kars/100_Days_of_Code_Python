9d63bc128ab4e39a35dedb27bae365a09e0f69f7
Bradley Kars <Bradley.kars@gmail.com>
2023-09-22 13:34:20 -0400

    Create 09-22-23 - Day 005 - If this...else that!.py

---
+++ Month 1 - September/Week 01 - (09-18-23 - 09-23-23)/09-22-23 - Day 005 - If this...else that!.py
@@ -0,0 +1,53 @@
+print("Which Z-fighter are you?")
+
+while True:
+    answer = input("Are you obsessed with cats? ")
+    if answer.lower() == "yes":
+        print("Oh you're Yajirobe!")
+        break
+    answer = input("Have you ever eaten so much people got mad at you? ")
+    if answer.lower() == "yes":
+        print("Oh you're Goku!")
+        break
+    answer = input("Do you have pointed ears? ")
+    if answer.lower() == "yes":
+        print("Oh you're Piccolo!")
+        break
+    answer = input("Did you go to college? ")
+    if answer.lower() == "yes":
+        print("Oh you're Gohan!")
+        break
+    answer = input("Do you wear a hat? ")
+    if answer.lower() == "yes":
+        print("Oh you're Chiaotzu!")
+        break
+    answer = input("Do you like wearing a shirt? ")
+    if answer.lower() == "no":
+        print("Oh you're Tien Shinhan!")
+        break
+    answer = input("Are you bald? ")
+    if answer.lower() == "yes":
+        print("Oh you're Krillin!")
+        break
+    answer = input("Are you a flirt? ")
+    if answer.lower() == "yes":
+        print("Oh you're Yamcha!")
+        break
+    answer = input("Do you have anger issues? ")
+    if answer.lower() == "yes":
+        print("Oh you're Vegeta!")
+        break
+    answer = input("Do you have daddy issues? ")
+    if answer.lower() == "yes":
+        print("Oh you're Trunks!")
+        break
+    answer = input("Are you a child? ")
+    if answer.lower() == "yes":
+        print("Oh you're Goten!")
+        break
+    answer = input("Are you an elderly person? ")
+    if answer.lower() == "yes":
+        print("Oh you're Roshi!")
+        break
+    print("Huh, you must be Hercule...")
+    break
