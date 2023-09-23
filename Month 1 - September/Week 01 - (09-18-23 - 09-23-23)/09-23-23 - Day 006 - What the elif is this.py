ff07e01641ae3fb2fad9d274b5b9ec3e75e02ccc
Bradley Kars <Bradley.kars@gmail.com>
2023-09-23 18:10:43 -0400

    Create 09-23-23 - Day 006 - What the elif is this.py

---
+++ Month 1 - September/Week 01 - (09-18-23 - 09-23-23)/09-23-23 - Day 006 - What the elif is this.py
@@ -0,0 +1,11 @@
+print("Secure Login:")
+username = input("Username: ")
+password = input("Password: ")
+if username == "Bradley" and password == "w0rdpass":
+  print("Welcome", username + ", I hope your having a wonderful day!")
+elif username == "John" and password == "12345678":
+  print("Hey", username + ", It's been a long time")
+elif username == "Charles" and password == "selrahC":
+  print("Long time no see", username)
+else:
+  print("That's my purse, I don't know you!")
