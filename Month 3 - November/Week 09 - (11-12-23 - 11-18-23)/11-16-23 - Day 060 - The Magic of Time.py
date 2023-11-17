4bcc671c2065a8df5d058347c991fd72ca0ea4c3
Bradley Kars <Bradley.kars@gmail.com>
2023-11-16 23:47:13 -0500

    Create 11-16-23 - Day 060 - The Magic of Time.py

---
+++ Month 3 - November/Week 09 - (11-12-23 - 11-18-23)/11-16-23 - Day 060 - The Magic of Time.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+print("Event Countdown")
+
+todays_date = datetime.today().date()
+
+event_date = input("When is the event, please eneter this in mm-dd-yyyy format: ")
+event_date = datetime.strptime(event_date, "%m-%d-%Y").date()
+
+time_difference = event_date - todays_date
+
+if int(time_difference.days) > 0:
+  print(f"Looks liek your event is in {time_difference.days} days.")
+elif int(time_difference.days) < 0:
+  print(f"Looks like that happaned {abs(time_difference.days)} days ago.")
+else:
+  print("Thats today!!!")
